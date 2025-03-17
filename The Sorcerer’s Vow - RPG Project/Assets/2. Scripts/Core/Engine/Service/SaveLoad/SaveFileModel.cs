808ecbab5a0d80d1a49d317d3aca47dee6d116a3
rafarvns <rafarvns@gmail.com>
2025-03-17 16:36:11 -0300

    feat: adicionar sistema de salvamento e carregamento de progresso

---
+++ The Sorcerer’s Vow - RPG Project/Assets/2. Scripts/Core/Engine/Service/SaveLoad/SaveFileModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using _2._Scripts.Core.Domain.Character.Player;
+
+namespace _2._Scripts.Core.Engine.Service.SaveLoad
+{
+    [System.Serializable]
+    public class SaveFileModel
+    {
+        public List<PlayerCharacter> Saves;
+        public static SaveFileModel Instance = new SaveFileModel();
+    }
+}
