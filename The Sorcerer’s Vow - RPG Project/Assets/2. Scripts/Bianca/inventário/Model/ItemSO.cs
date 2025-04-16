d4f25b235f6e7c8c9c3c9f5c2c915fcf8e668263
Bianca Dellevedove Matsumura <bdmatsumura@sga.pucminas.br>
2025-04-16 19:43:45 -0300

    Testando Sistema de Craft

--- The Sorcerer’s Vow - RPG Project/Assets/2. Scripts/Bianca/inventário/Model/ItemSO.cs
+++ The Sorcerer’s Vow - RPG Project/Assets/2. Scripts/Bianca/inventário/Model/ItemSO.cs
@@ -24,43 +24,37 @@ public enum Frutos
 }
 
     public abstract class ItemSO : ScriptableObject
-{     
-    [field: SerializeField]
-    public string Name { get; set; }
+    {     
+        [field: SerializeField]
+        public string Name { get; set; }
 
-    [field: SerializeField]
-    [field: TextArea]
-    public string Description { get; set; }
-
-    [SerializeField]
-    private ItemCategory category;
-
-    public ItemCategory Category => category;
-
-    [field: SerializeField]
-    public bool IsStackable { get; set; }
-
-    [field: SerializeField]
-    public int MaxStackSize { get; set; } = 1;
-
-    public int ID => GetInstanceID();
-
-    [field: SerializeField]
-    public Sprite ItemImage { get; set; }
-
-    [field: SerializeField]
-    public Mesh ItemMesh { get; set; }
+        [field: SerializeField]
+        [field: TextArea]
+        public string Description { get; set; }
         
-    [field: SerializeField]
-    public Material ItemMaterial { get; set; }
+        [field: SerializeField]
+        public GameObject WorldPrefab { get; private set; }
 
-    [field: SerializeField]
-    public List<ItemParameter> DefaultParametersList { get; set; }
-    [field: SerializeField]
-    public GameObject WorldPrefab { get; private set; }
+        [SerializeField]
+        private ItemCategory category;
 
+        public ItemCategory Category => category;
 
-}
+        [field: SerializeField]
+        public bool IsStackable { get; set; }
+
+        [field: SerializeField]
+        public int MaxStackSize { get; set; } = 1;
+
+        public int ID => GetInstanceID();
+
+        [field: SerializeField]
+        public Sprite ItemImage { get; set; }
+
+        [field: SerializeField]
+        public List<ItemParameter> DefaultParametersList { get; set; }
+
+    }
 
     [Serializable]
     public struct ItemParameter : IEquatable<ItemParameter>
