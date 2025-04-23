d4c0d1c6e607fa8ce46454390942216fbef1dfef
Bianca Dellevedove Matsumura <bdmatsumura@sga.pucminas.br>
2025-04-22 23:12:39 -0300

    Modificação de atributos do Player através dos Frutos

--- The Sorcerer’s Vow - RPG Project/Assets/2. Scripts/Core/Domain/Character/Player/PlayerCharacter.cs
+++ The Sorcerer’s Vow - RPG Project/Assets/2. Scripts/Core/Domain/Character/Player/PlayerCharacter.cs
@@ -1,22 +1,47 @@
 ﻿using System;
+using System.Collections;
 using _2._Scripts.Core.Domain.Character.Base;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _2._Scripts.Core.Domain.Character.Player
 {
     [Serializable]
     public class PlayerCharacter : BaseCharacter
     {
+        [SerializeField] private Slider sliderLife;
+       
+
         public override void Start()
         {
             base.Start();
+            sliderLife.maxValue = maxHealth;
+            sliderLife.value = currentHealth;
+
             Debug.Log("PlayerCharacter Start");
         }
 
         public override void Update()
         {
             base.Update();
+            sliderLife.value = currentHealth;
             // Debug.Log("PlayerCharacter Update");
         }
+
+        public void ChangeVelocityTemporarily(int newSpeed, float duration)
+        {
+            StopCoroutine(nameof(ResetSpeedCoroutine)); // previne sobreposição
+            StartCoroutine(ResetSpeedCoroutine(newSpeed, duration));
+        }
+
+        private IEnumerator ResetSpeedCoroutine(int newSpeed, float duration)
+        {
+            int previousSpeed = Speed;
+            ChangeVelocity(newSpeed);
+            yield return new WaitForSeconds(duration);
+            ChangeVelocity(previousSpeed);
+        }
+
+        
     }
 }
