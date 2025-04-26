8eea24131a9614fff1262fb15dfc36a6d800b8cc
Bianca Dellevedove Matsumura <bdmatsumura@sga.pucminas.br>
2025-04-26 17:26:01 -0300

    finalização Etapa 3

---
+++ The Sorcerer’s Vow - RPG Project/Assets/2. Scripts/Bianca/shopSystem/ShopSO.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Inventory.Model;
+using UnityEngine;
+
+namespace ShopSystem
+{
+    [CreateAssetMenu(menuName = "Shop/ShopSO")]
+    public class ShopSO : ScriptableObject
+    {
+        [SerializeField]
+        private List<ShopItem> shopItems;
+
+        public List<ShopItem> ShopItems => shopItems;
+    }
+
+    [System.Serializable]
+    public struct ShopItem
+    {
+        public ItemSO item;
+        public int price; // preço de compra (e talvez preço de venda também)
+    }
+}
