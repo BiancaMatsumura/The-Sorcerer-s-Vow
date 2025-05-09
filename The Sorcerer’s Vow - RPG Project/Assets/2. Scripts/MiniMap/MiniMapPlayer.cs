65668dc9f9425c8b9d1ec2a19261ea3d1dddc140
jonfpassos <jonfpassos@hotmail.com>
2025-05-08 23:20:01 -0300

    Feat: Criacao do mini mapa

---
+++ The Sorcerer’s Vow - RPG Project/Assets/2. Scripts/MiniMap/MiniMapPlayer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MiniMapPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+}
